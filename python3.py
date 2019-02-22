b8390148352251bd8bdc979566200cbeef55fa59
shebilantony <42166928+shebilantony@users.noreply.github.com>
2019-02-22 14:05:12 +0530

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,5 +1,5 @@
 char = input("enter a character:")
 if((char>='a' and char<='b') or (char>='A' and char<='B')):
-  print("char is an Alphabet)
+  print("char is an Alphabet")
 else:
   print("char is not an Alphabet")
